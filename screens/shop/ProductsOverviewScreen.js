40c137712a6bc4c5ee325bda0108611b35d70b8f
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-04-01 07:56:15 -0700

    set up products screen and reducer

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = props => {
-  return <FlatList />;
+  const products = useSelector(state => state.products.availableProducts);
+
+  const renderProductItem = itemData => {
+    return (
+      <TouchableOpacity onPress={() => console.log(itemData.item)}>
+        <Text>{itemData.item.title}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={renderProductItem}
+    />
+  );
 };
 
 export default ProductsOverviewScreen;
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,19 +1,18 @@
-import PRODUCTS from '../../data/dummy-data';
+import PRODUCTS from "../../data/dummy-data";
 
 const initialState = {
-    availableProducts = PRODUCTS,
-    userProducts = PRODUCTS.filter(product => product.ownderId == 'u1 ')
+  availableProducts: PRODUCTS,
+  userProducts: PRODUCTS.filter(product => product.ownderId == "u1 ")
 };
 
 const productsReducer = (state = initialState, action) => {
-    switch(action.type) {
+  switch (action.type) {
+    case "":
+      return state;
 
-        case '':
-            return state;
-
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default productsReducer;
