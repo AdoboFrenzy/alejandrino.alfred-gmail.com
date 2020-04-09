6d4297330c4b9ca6b379e29e01190946f3b412f5
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-04-09 05:29:11 -0700

    add add to cart headerbutton

---
+++ components/UI/HeaderButton.js
@@ -0,0 +1,8 @@
+import { Ionicons } from "@expo/vector-icons";
+import { HeaderButton } from "react-navigation-header-buttons";
+
+const HeaderButton = (props) => {
+  return;
+};
+
+export default HeaderButton;
