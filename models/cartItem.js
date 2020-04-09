5785f92601efdc9eae6028b57657507d489eb107
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-04-09 04:39:00 -0700

    set up cartsReducer

---
+++ models/cartItem.js
@@ -0,0 +1,10 @@
+class CartItem {
+  constructor(quantity, productTitle, productCost, totalCost) {
+    this.quantity = quantity;
+    this.productTitle = productTitle;
+    this.productCost = productCost;
+    this.totalCost = totalCost;
+  }
+}
+
+export default CartItem;
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,16 +1,37 @@
 import { ADD_TO_CART } from "../actions/cart";
 
+import CartItem from "../../models/cartItem";
+
 const initialState = {
   items: {},
   totalSum: 0,
 };
 
 const cartsReducer = (state = initialState, actions) => {
-  console.log(actions);
-
   switch (actions.type) {
     case ADD_TO_CART:
-      return state;
+      let updateOrNewCartItem;
+      const { id, title, price } = actions.product;
+
+      if (!state.items[id]) {
+        updateOrNewCartItem = new CartItem(1, title, price, price);
+      } else {
+        const { quantity, productTitle, productCost, totalCost } = state.items[
+          id
+        ];
+        updateOrNewCartItem = new CartItem(
+          quantity + 1,
+          productTitle,
+          productCost,
+          totalCost + productCost
+        );
+      }
+
+      return {
+        ...state,
+        items: { ...state.items, [id]: updateOrNewCartItem },
+        totalSum: state.totalSum + price,
+      };
 
     default:
       return state;
