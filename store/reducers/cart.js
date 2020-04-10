158d59d1e06042b25a7250e3b399c28e2315b297
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-04-10 08:20:18 -0700

    add removeCartItem feature

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -11,7 +11,7 @@ const cartReducer = (state = initialState, actions) => {
   switch (actions.type) {
     case ADD_TO_CART:
       let updateOrNewCartItem;
-      const { id, title, price } = actions.product;
+      var { id, title, price } = actions.product;
 
       if (!state.items[id]) {
         updateOrNewCartItem = new CartItem(1, title, price, price);
@@ -35,8 +35,30 @@ const cartReducer = (state = initialState, actions) => {
 
     case REMOVE_FROM_CART:
       // console.log(actions.product);
-      console.log("removing " + actions.product.title);
-      return state;
+      // console.log("removing " + actions.product.title);
+
+      let updatedCartItem;
+      var { id, title, price } = actions.product;
+
+      console.log(id);
+
+      if (state.items[id].quantity > 1) {
+        console.log("delete qty > 2");
+        // return {
+        //   ...state,
+        //   items: { ...state.items, [id]: updatedCartItem },
+        //   totalSum: state.totalSum - price,
+        // };
+        return state;
+      } else {
+        const updatedItems = delete { ...state.items }[id];
+
+        return {
+          ...state,
+          items: updatedItems,
+          totalSum: state.totalSum - price,
+        };
+      }
 
     default:
       return state;
