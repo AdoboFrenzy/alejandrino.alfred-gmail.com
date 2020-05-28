75cf60efcf3af54e0952f29a079f7daacbbaec48
AdoboFrenzy <alejandrino.alfred@gmail.com>
2020-05-28 09:59:19 -0700

    add startup screen for auto login

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -14,6 +14,7 @@ import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
 
 import AuthScreen from "../screens/user/AuthScreen";
+import StartupScreen from "../screens/user/StartupScreen";
 
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
@@ -103,6 +104,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const ShopNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopDrawerNavigator,
 });
